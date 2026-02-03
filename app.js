2a9726e5724e04ae6d6814f28a46a008930ab7be
belajarngaji <ffarid.5213@gmail.com>
2026-02-03 15:46:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // Konfigurasi Supabase
-const SUPABASE_URL = 'https://jpxtbdawajjyrvqrgijd.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpweHRiZGF3YWpqeXJ2cXJnaWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMTI4OTgsImV4cCI6MjA3MTg4ODg5OH0.vEqCzHYBByFZEXeLIBqx6b40x6-tjSYa3Il_b2mI9NE';
+const SUPABASE_URL = 'https://gmmhsbmlqbomtrhdoxxo.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_r0gMojctdN1aftj_PVuhAQ_R0s__keH';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
